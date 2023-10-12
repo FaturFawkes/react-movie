a7aae1c06930c0804e922ec06fbdbeb589b41cdc
faturfawkes <nur.faturohman28@gmail.com>
2023-10-12 16:49:19 +0700

    data fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
-import { useState } from 'react'
+import { Component, ReactNode, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
-import Movie from './assets/dummy.json'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className='w-full h-[2000px] bg-Eerie-black'>
-        <Navbar/>
-        <Hero/>
-          <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto max-w-7x1">
-            <div className="flex flex-wrap w-full mb-4 p-4">
-              <div className="w-full mb-6 lg:mb-0">
-                <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-white">Box Office</h1>
-                <div className="h-1 w-90 bg-indigo-500 rounded"></div>
+interface DataState {
+  data: []
+}
+class App extends Component<DataState> {
+  state = {
+    data: [],
+  }
+
+  baseUrl = import.meta.env.VITE_BASE_URL 
+  apiKey = import.meta.env.VITE_API_KEY
+
+  async handleMovie () {
+    axios.get(`${this.baseUrl}/movie/upcoming?api_key=${this.apiKey}`)
+    .then((response) => {
+      console.log(response.data.results)
+      this.setState({data: response.data.results})
+    })
+    .catch((err) => {
+      console.log(err)
+    })  
+  }
+
+  componentDidMount(): void {
+    this.handleMovie()
+  }
+
+  render() {   
+    const { data } = this.state
+
+    return (
+      <>
+        <div className='w-full h-[2000px] bg-Eerie-black'>
+          <Navbar/>
+          <Hero/>
+            <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto max-w-7x1">
+              <div className="flex flex-wrap w-full mb-4 p-4">
+                <div className="w-full mb-6 lg:mb-0">
+                  <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-white">Box Office</h1>
+                  <div className="h-1 w-90 bg-indigo-500 rounded"></div>
+                </div>
+              </div>
+              <div className="flex flex-wrap -m-4">
+                {
+                  data.map((item: any, index) => {
+                    return (
+                      <Card  key={index} image={item.backdrop_path} desc={item.overview} title={item.title}/>
+                    )
+                  })
+                }
               </div>
             </div>
-            <div className="flex flex-wrap -m-4">
-          {
-            Movie.map((item: any) => {
-              return (
-                <Card  id={item.id} image={item.image}/>
-              )
-            })
-          }
-                      </div>
-          </div>
-        </section>
-      </div>
-    </>
-  )
+          </section>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,28 @@
 import React, { Component } from 'react'
 
 interface Movie{
-    id: number
     image: string
+    title: string
+    desc: string
 } 
 export class Card extends Component<Movie> {
   render() {
-    const {image} =  this.props
+    const {image, title, desc} =  this.props
     return (    
         // <div className="card lg:w-[245px] lg:h-[140px] md:w-[205px] lg:h-[140px] mx-4 my-2 bg-Night p-0 rounded-none shadow-xl hover:scale-125 transition">
         //     <figure><img src={`${image}`} alt="Shoes" /></figure>
         // </div>
 
-        <div className="xl:w-1/3 md:w-1/2 p-4 hover:z-[99]">
-          <div className="rounded-lg group hover:scale-125 transition duration-[1s]">
-            <img className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={`${image}`} alt="Image Size 720x400"/>
-            <div className='text-lg text-gray-900 bg-white mt-[-23px] font-medium title-font absolute mb-4 hidden transition group-hover:block'>
-              <h2 className="">Chichen Itza</h2>
-              <p className="leading-relaxed text-base hidden transition group-hover:block ">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <div className="xl:w-1/3 md:w-1/2 p-4 hover:z-[1]">
+          <div className="rounded-lg group hover:scale-[1.1] transition duration-[.7s]">
+            <img 
+              className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" 
+              src={`https://image.tmdb.org/t/p/w500/${image}`} 
+              alt="Image Size 720x400"
+            />
+            <div className='text-lg text-gray-900 w-[391px] p-[10px] bg-gray-200 mt-[-24px] font-medium title-font absolute mb-4 hidden transition group-hover:block rounded'>
+              <h2 className="">{`${title}`}</h2>
+              <p className="leading-relaxed text-base text-[12px] hidden transition group-hover:block ">{`${desc}`}</p>
             </div>
           </div>
         </div>
